851508f6375ab9c29e3c541eac671b152c0f101c
Siyu Liu <siyu.java.web@gmail.com>
2022-12-04 22:44:33 +1100

    polishing the style and clean the codes

--- Assets/js/quiz-script.js
+++ Assets/js/quiz-script.js
@@ -4,8 +4,6 @@ var quizNumber = 0;
 var finish = false;
 var score;
 
-
-
 //get element from html
 var timer = document.querySelector("#timer");
 var quiz = document.querySelector("#quiz");
@@ -16,8 +14,6 @@ var optionC = document.querySelector("#optionC");
 var optionD = document.querySelector("#optionD");
 var message = document.querySelector("#message");
 
-var submit;
-
 //create objects for each quiz
 var question1 = {
     content: "1. Inside which HTML element do we put the JavaScript?",
@@ -27,7 +23,6 @@ var question1 = {
     optionD: "<js>",
     correctAnswer: "<script>"
 };
-
 var question2 = {
   content: "2. How do you write 'Hello World' in an alert box?",
   optionA: "alertBox('Hello World')",
@@ -36,7 +31,6 @@ var question2 = {
   optionD: "alert('Hello World')",
   correctAnswer: "alert('Hello World')",
 };
-
 var question3 = {
   content: "3. How to write an IF statement in JavaScript?",
   optionA: "if i==5 then",
@@ -45,7 +39,6 @@ var question3 = {
   optionD: "if (i==5)",
   correctAnswer: "if (i==5)",
 };
-
 var question4 = {
   content: "4. Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
   optionA: "Position",
@@ -54,7 +47,6 @@ var question4 = {
   optionD: "Location",
   correctAnswer: "Window",
 };
-
 var question5 = {
   content: "5. Which of the following is true about variable naming conventions in JavaScript?",
   optionA: "JavaScript variable names must begin with a letter or the underscore character",
@@ -63,7 +55,6 @@ var question5 = {
   optionD: "None of the above",
   correctAnswer: "Both of the above",
 };
-
 var question6 = {
   content: "6. Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
   optionA: "last()",
@@ -72,7 +63,6 @@ var question6 = {
   optionD: "None of the above",
   correctAnswer: "push()",
 };
-
 var question7 = {
   content:
     "7. Which of the following function of String object returns the characters in a string between two indexes into the string?",
@@ -82,7 +72,6 @@ var question7 = {
   optionD: "substring()",
   correctAnswer: "substring()",
 };
-
 var question8 = {
   content: "8. Which of the following function of String object returns the calling string value converted to lower case?",
   optionA: "toLocaleLowerCase()",
@@ -94,6 +83,17 @@ var question8 = {
 
 //create array for the quiz objects
 var quizArray = [question1, question2, question3,question4,question5,question6,question7,question8]
+
+//initial function
+function init() { 
+    timeLeft = 60;
+    quiz.textContent = quizArray[quizNumber].content;
+    optionA.textContent = quizArray[quizNumber].optionA;
+    optionB.textContent = quizArray[quizNumber].optionB;
+    optionC.textContent = quizArray[quizNumber].optionC;
+    optionD.textContent = quizArray[quizNumber].optionD;
+    timeCountDown();   
+}
 
 //timer function
 function timeCountDown() { 
@@ -113,29 +113,13 @@ function timeCountDown() {
     }, 1000);  
 }
 
-//function of showing the finishing page
-function addLinkToResultPage() { 
-    score = timeLeft;
-    var buttons = document.querySelectorAll("button");
-    for (i = 0; i < buttons.length; i++) { 
-        buttons[i].setAttribute("onclick","window.location.href='result.html'");
-    }
-}
-
-//function of changing to the next quiz
-function nextPage() { 
-    
+//function of display the next quiz or jump to the result page if reach the last quiz
+function nextPage() {     
     if (quizNumber == quizArray.length) {
-    //   quiz.textContent = quizArray[quizNumber].content;
-    //   optionA.textContent = quizArray[quizNumber].optionA;
-    //   optionB.textContent = quizArray[quizNumber].optionB;
-    //   optionC.textContent = quizArray[quizNumber].optionC;
-    //   optionD.textContent = quizArray[quizNumber].optionD;
-      // addLinkToResultPage();
-      finish = true;
-      score = timeLeft;
-      localStorage.setItem("score", score);
-      window.location.href = "result.html";
+        finish = true;
+        score = timeLeft;
+        localStorage.setItem("score", score);
+        window.location.href = "result.html";
     } else {
         quiz.textContent = quizArray[quizNumber].content;
         optionA.textContent = quizArray[quizNumber].optionA;
@@ -145,43 +129,17 @@ function nextPage() {
     }  
 } 
 
-//event of clicking one option
+//event of clicking one option then turn to the next question
 var clickEvent = function (event) {
-  if (event.target.textContent == quizArray[quizNumber].correctAnswer) {
-    message.textContent = "Correct!";
-  } else {
-    message.textContent = "Wrong!";
-    timeLeft -= 10;
-  }
-    
-    // if (quizNumber == quizArray.length) {
-    //   finish = true;
-    //   score = timeLeft;
-    //   localStorage.setItem("score", score);
-    // } 
+    if (event.target.textContent == quizArray[quizNumber].correctAnswer) {
+        message.textContent = "Correct!";
+    } else {
+        message.textContent = "Wrong!";
+        timeLeft -= 10;
+    }
     quizNumber++;
-  nextPage();
-  
+    nextPage();
 };
-    
+
 options.addEventListener("click", clickEvent);
-
-//render function
-function init() { 
-    timeLeft = 10;
-    quiz.textContent = quizArray[quizNumber].content;
-    optionA.textContent = quizArray[quizNumber].optionA;
-    optionB.textContent = quizArray[quizNumber].optionB;
-    optionC.textContent = quizArray[quizNumber].optionC;
-    optionD.textContent = quizArray[quizNumber].optionD;
-    timeCountDown();   
-}
-
 init();
-
-
-
-//get initial and score and save it to the localstorage when clicking submit
-
-
-
